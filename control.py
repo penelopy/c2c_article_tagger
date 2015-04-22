1afda2f8163a790c70f839272e120d2b903588c4
Penelope Hill <penelope.codes@gmail.com>
2015-04-22 11:03:49 -0700

    Interface working, working on writing to tags table

--- control.py
+++ control.py
@@ -1,30 +1,21 @@
 from flask import Flask, render_template,request
-# from flask import session as flask_session
 import os
-# import model
-# import csv
+from model import con
 
 
-import sqlite3 as lite
-import sys
-import codecs
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
 
 article_data_list = []
-con = lite.connect('test.db')
 
 with con:    
-    
     cur = con.cursor()    
     cur.execute("SELECT * FROM Feeds")
 
     rows = cur.fetchall()
     for row in rows: 
         article_data_list.append(row)
-
-
-app = Flask(__name__)
-
-app.secret_key = os.environ.get("SECRET_KEY")
 
 
 list_of_article_objects = []
@@ -35,8 +26,15 @@ class Article:
         self.url = url
         self.tags = []
 
+
+# class Tag: 
+#     def __init__(self, tag_name):
+#         self.tag_name = tag_name
+#         self.url = url
+
 article_summarys = []
 article_urls = []
+list_of_tag_objects = []
 
 
 for article in article_data_list:
@@ -45,7 +43,7 @@ for article in article_data_list:
         article_summarys.append(article[3])
 
     if article[0] is not None:
-        url = article[0].encode('utf-8')
+        url = article[1].encode('utf-8')
         article_urls.append(url)
 
 for i in range(len(article_summarys)):
@@ -58,28 +56,36 @@ article0 = list_of_article_objects[0]
 @app.route('/', methods=["GET", "POST"])
 def home_page(): 
     possible_tags1 = ["Supporting Arts & Culture", "Improving Education", "Protecting the Environment", "Animal Welfare", "Improving Health Care", "Women's Health", "Mental Health"]
-    possible_tags2 =["Suicide Prevention", "Diseases", "Curing Cancer", "Curing Breast Cancer", "HIV & Aids", "Autism", "Children's Diseases"]
+    possible_tags2 = ["Suicide Prevention", "Diseases", "Curing Cancer", "Curing Breast Cancer", "HIV & Aids", "Autism", "Children's Diseases"]
     possible_tags3 = ["Justice", "Trafficking and Exploitation", "Increasing Jobs", "Feeding the Hungry", "Improving Nutrition", "Affordable Housing", "Disaster Relief"]
     possible_tags4 = ["Recreation & Fitness", "At-Risk Youth", "Domestic Violence Shelters & Services", "Supporting Services for Seniors", "International Economic Development", "International Disaster Relief", "International Social Justice"]
     possible_tags5 = ["Civil Rights & Liberties", "Women's Rights", "LGBT Rights", "Reproductive Rights", "Community Economic Development", "Advancing Science & Technology", "Services for Vets"]    
-    # url_prev = request.args.get('id1')
     url = request.args.get('id2')
     url_prev = request.args.get('id1')
+    tag = request.form.get('id')
+    if tag is not None:
+        list_of_tag_objects.append(tag)
+
 
     if url is None and url_prev is None:
-        return render_template("index.html", article=article0, first_tags=possible_tags1, second_tags=possible_tags2, third_tags=possible_tags3, fourth_tags=possible_tags4, fifth_tags=possible_tags5)
+        return render_template("index.html", article=article0, first_tags=possible_tags1, second_tags=possible_tags2, third_tags=possible_tags3, fourth_tags=possible_tags4, fifth_tags=possible_tags5, list_of_tag_objects=list_of_tag_objects)
+
     else:
         if url is not None:
-            # index = article_urls.index(url)
-            # if index > last_index: 
-            print "next= ", url
             article1 = next_article(url)
-            # print "new article url", article1.url
-            return render_template("index.html", article=article1, first_tags=possible_tags1, second_tags=possible_tags2, third_tags=possible_tags3, fourth_tags=possible_tags4, fifth_tags=possible_tags5)
+            for i in range(len(list_of_tag_objects)):
+                # item = list_of_tag_objects.pop()
+                # PK = cur.execute("SELECT MD5 from feeds WHERE link = url")
+                # cur.execute("INSERT INTO tags (MD5, link, tag_name) VALUES (PK, url, 'suicide')")
+                # cur.commit()
+                # article1.tags.append(item)
+            # print "a= ", article1.tags
+            # print "b= ", list_of_tag_objects
+            return render_template("index.html", article=article1, first_tags=possible_tags1, second_tags=possible_tags2, third_tags=possible_tags3, fourth_tags=possible_tags4, fifth_tags=possible_tags5, list_of_tag_objects=list_of_tag_objects)
+
         elif url_prev is not None:
-            print "prev= ", url_prev
             article1 = prev_article(url_prev)
-            return render_template("index.html", article=article1, first_tags=possible_tags1, second_tags=possible_tags2, third_tags=possible_tags3, fourth_tags=possible_tags4, fifth_tags=possible_tags5)
+            return render_template("index.html", article=article1, first_tags=possible_tags1, second_tags=possible_tags2, third_tags=possible_tags3, fourth_tags=possible_tags4, fifth_tags=possible_tags5, list_of_tag_objects=list_of_tag_objects)
 
 
 def next_article(url):
@@ -87,7 +93,6 @@ def next_article(url):
         index = article_urls.index(url)
         next_index = index + 1
         article = list_of_article_objects[next_index]
-        # print "next url", article.url
         return article
 
 def prev_article(url):
@@ -95,12 +100,7 @@ def prev_article(url):
         index = article_urls.index(url)
         prev_index = index - 1
         article = list_of_article_objects[prev_index]
-        # print "next url", article.url
         return article
-
-
-    # if id is not None:
-    #     article.tags.append(id)
 
 
 
--- model.py
+++ model.py
@@ -2,17 +2,43 @@ import sqlite3 as lite
 # import sys
 # import codecs
 
-article_data_list = []
 con = lite.connect('test.db')
 
-with con:    
-    
-    cur = con.cursor()    
-    cur.execute("SELECT * FROM Feeds")
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import create_engine, ForeignKey
+# from sqlalchemy import Column, Integer, String 
+# from sqlalchemy.orm import sessionmaker, relationship, backref
 
-    rows = cur.fetchall()
-    for row in rows: 
-        article_data_list.append(row)
+# ENGINE = None
+# Session = None
+
+# Base = declarative_base()
+
+
+# class Article():
+#     __tablename__ = "articles"
+#     id = Column(String(200), primary_key = True, nullable=False)
+#     title = Column(String(50), nullable = False)
+#     summary = Column(String(400), nullable = False)
+#     rss_source_url = Column(String(200), nullable = False)
+#     published = Column(String(50), nullable = False)
+#     timestamp = Column(String(50), nullable = False)
+
+# class Tag():
+#     __tablename__ = "tags"
+#     id = Column(Integer(6), primary_key = True, nullable=False)
+#     tag_name = Column(String(32), nullable = False)
+
+# articles_tags = Table('articles_tags', Base.metadata, 
+#     Column('article_tag_id', Integer, primary_key=True, nullable=False), 
+#     Column('link', String, ForeignKey('articles.link'), nullable=False),
+#     Column('tags_id', Integer, ForeignKey('tags.id'), nullable=False)
+#     )
+
+
+
+
+
 # return article_data_list
 
 
@@ -24,30 +50,8 @@ with con:
 
 # save_output_to_txt_file(rows, 'complete_article_list.txt')    
 
-    # for row in rows:
-    #     print "******************"
-    #     print "ARTICLE DATA"
-
-    #     # for item in row: 
-    #     #     print item
 
 
-    #     link = row[0]
-    #     title = row[1]
-    #     summary = row[2]
-    #     rss_source_url = row[3]
-    #     published = row[4]
-    #     timestamp = row[5]
-
-        # print link
-        # print title
-
-#     link = Column(String(32), primary_key = True, nullable = False)
-#     title = Column(String(32), nullable = False)
-#     summary = Column(String(32), nullable = False)
-#     rss_source_url = Column(String(32), nullable = False)
-#     published = Column(String(32), nullable = False)
-#     timestamp = Column(String(32), nullable = False)
 
 
 # cur.execute("INSERT INTO Friends(Name) VALUES ('Tom');")
@@ -55,40 +59,22 @@ with con:
 #  cur.execute("CREATE TABLE Friends(Id INTEGER PRIMARY KEY, Name TEXT);")
 
 
-class Article():
-    __tablename__ = "articles"
-    id = Column(String(32), primary_key = True, nullable=False)
-    title = Column(String(32), nullable = False)
-    summary = Column(String(32), nullable = False)
-    rss_source_url = Column(String(32), nullable = False)
-    published = Column(String(32), nullable = False)
-    timestamp = Column(String(32), nullable = False)
-
-class Tag():
-    __tablename__ = "tags"
-    id = Column(Integer(6), primary_key = True, nullable=False)
-    tag_name = Column(String(32), nullable = False)
-
-articles_tags = Table('articles_tags', Base.metadata, 
-    Column('article_tag_id', Integer, primary_key=True, nullable=False), 
-    Column('link', String, ForeignKey('articles.link'), nullable=False),
-    Column('tags_id', Integer, ForeignKey('tags.id'), nullable=False)
-    )
 
 
-def connect():
-    global ENGINE
-    global Session
-    ENGINE = create_engine("sqlite:///test.db", echo=True)
-    Session = sessionmaker(bind=ENGINE)
-    return Session()
 
-db_session = connect()
+# def connect():
+#     global ENGINE
+#     global Session
+#     ENGINE = create_engine("sqlite:///test.db", echo=True)
+#     Session = sessionmaker(bind=ENGINE)
+#     return Session()
 
-def main():
-    connect()
-    Base.metadata.create_all(ENGINE)
+# db_session = connect()
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     connect()
+#     Base.metadata.create_all(ENGINE)
+
+# if __name__ == "__main__":
+#     main()
 
